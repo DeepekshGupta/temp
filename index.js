d6bdefc92dca598e8668bf4251d677c213ca3862
Deepeksh <deepeksh77673@gmail.com>
2022-03-04 12:53:39 +0530

    Procfile

--- index.js
+++ index.js
@@ -73,8 +73,7 @@ app.get("/complete",function(req,res){
 
 // Take any port number of your choice which
 // is not taken by any other process
-app.listen(8080,function(error) {
-	if(error) throw error
-		console.log("Server created Successfully on PORT 8080")
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running @ 3000");
 
+})
