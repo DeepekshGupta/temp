86d2b45d37c43c1c3e8500bfe803a25422973fb0
Deepeksh <deepeksh77673@gmail.com>
2022-03-04 12:48:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const path = require("path")
+const multer = require("multer")
+const app = express()
+var fs = require('fs');
+
+// View Engine Setup
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs")
+	
+// var upload = multer({ dest: "Upload_folder_name" })
+// If you do not want to use diskStorage then uncomment it
+
+var fname = "";
+upload_folder = "uploads"
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+
+		// Uploads is the Upload_folder_name
+		cb(null, upload_folder)
+	},
+	filename: function (req, file, cb) {
+		fname = file.fieldname + "-" + Date.now()+file.originalname;
+	cb(null, file.fieldname + "-" + Date.now()+file.originalname)
+	}
+})
+	
+
+	
+var upload = multer({storage: storage}).single("mypic");	
+
+app.get("/",function(req,res){
+	res.render("Signup");
+})
+	
+app.post("/uploadProfilePicture",function (req, res, next) {
+		
+	// Error MiddleWare for multer file upload, so if any
+	// error occurs, the image would not be uploaded!
+	upload(req,res,function(err) {
+
+		if(err) {
+
+			// ERROR occured (here it can be occured due
+			// to uploading image of size greater than
+			// 1MB or uploading different file type)
+			res.send(err)
+		}
+		else {
+
+			// SUCCESS, image successfully uploaded
+			fname = __dirname + "\\" + upload_folder + "\\" + fname;
+
+			res.redirect("/complete")
+		}
+	})
+})
+	
+function delete_file(path){
+	console.log(fs.readdirSync(__dirname + "\\" + upload_folder + "\\")); 
+	var filePath = path;
+    fs.unlinkSync(filePath);
+}
+
+app.get("/complete",function(req,res){
+	// delete_file(fname);
+	delete_file(fname);
+    res.send("hello")
+    // res.render("Signup");
+})
+
+
+// Take any port number of your choice which
+// is not taken by any other process
+app.listen(8080,function(error) {
+	if(error) throw error
+		console.log("Server created Successfully on PORT 8080")
+})
+
